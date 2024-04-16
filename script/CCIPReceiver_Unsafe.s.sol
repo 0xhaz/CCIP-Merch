b27e473588c0b82fc03e2acfb80585a0f8363597
0xhaz <28639279+0xhaz@users.noreply.github.com>
2024-04-17 04:30:29 +0800

    Added Deploy Script

---
+++ script/CCIPReceiver_Unsafe.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.19;
+
+import "forge-std/Script.sol";
+import {CCIPReceiver_Unsafe} from "../src/CCIPReceiver_Unsafe.sol";
+
+contract DeployCCIPReceiver_Unsafe is Script {
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        address sepoliaRouter = 0xD0daae2231E9CB96b94C8512223533293C3693Bf;
+
+        CCIPReceiver_Unsafe receiver = new CCIPReceiver_Unsafe(sepoliaRouter);
+
+        console.log("CCIPReceiver_Unsafe deployed at: ", address(receiver));
+
+        vm.stopBroadcast();
+    }
+}
